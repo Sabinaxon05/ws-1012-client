4e6c19663aaa973d136535a3ea4fbe2d63ec2e9e
Sabinaxon05 <sabinaabdugaffarova927@gmail.com>
2025-04-28 16:14:59 +0500

    ASD

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,7 @@ import viteLogo from '/vite.svg'
 import './App.css'
 import io from "socket.io-client"
 
-const socket = io("http://localhost:8000")
+const socket = io("https://ws-1012-game-server.onrender.com")
 
 function App() {
   const [count, setCount] = useState(0)
